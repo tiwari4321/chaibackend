a6521b03b7dc601b762451a64feb8375adf4118d
Abhinav Tiwari <abhinavtiwari4320@gmail.com>
2024-06-15 18:09:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/twitter',(req,res)=>{
+    res.send('abhinavdotcom')
+})
+
+app.get('/login',(req,res)=>{
+  res.send('<h1>Please login at Chai and Code</h1>');
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
